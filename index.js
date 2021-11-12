b95b8e449cf94f3b7bc5ba27e22ed043f787f59c
pujaKundu <iampujakundu@gmail.com>
2021-11-12 12:54:30 +0600

    admin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const ObjectId = require("mongodb").ObjectId;
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 const { MongoClient } = require("mongodb");
 
 require("dotenv").config();
